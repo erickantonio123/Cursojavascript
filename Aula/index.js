1aa959a423a6eb6b8c59febee617dc5fddbad30c
erickantonio123 <erickapmercado@hotmail.com>
2025-10-29 15:42:26 -0300

    Treino de string

--- Aula/index.js
+++ Aula/index.js
@@ -1,6 +1,21 @@
-alert('OLA');
 
-let varA = 'A'
-let varB = 'B'
-let varC = 'C'
+
+let varA = 'A' //c
+let varB = 'B'//b
+let varC = 'C'//a
+
+
+const b = varB;
+
+
+varB= varC //a
+
+const a = varA
+
+varA = a //c
+
+varC = b//b
+
+
+console.log(varA,varB,varC)
 
---
+++ Aula/teststring.js
@@ -0,0 +1,7 @@
+let umastring = " ola teste ou";
+
+console.log(umastring[5])   
+console.log(umastring.charAt(3)); 
+console.log(umastring.charAt(5))
+console.log(umastring.charAt(12))
+console.log(`${umastring} vai dar certo`)
