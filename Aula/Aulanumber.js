2f639964ab33c6fc3be177e5425072fd5e0108d7
erickantonio123 <erickapmercado@hotmail.com>
2025-10-31 10:55:44 -0300

    Exercicio feito e novo exercicio sobre Number

---
+++ Aula/Aulanumber.js
@@ -0,0 +1,13 @@
+let number1 =10.52825654;
+let number2 =1.5
+
+let sum = number1+number2;
+
+console.log(number1.toString() + number2)//junçao
+num1= number1.toString();// conversão de num para string
+
+//console.log(number1.toString(2)) conversão binaria
+//console.log(typeof num1) verificar o tipo
+console.log(`Resultado ${number1.toFixed(2)}`)
+
+console.log(sum)
